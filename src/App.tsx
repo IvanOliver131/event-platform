afa6b96eb71508fe2be1116e3a04fee936c1d261
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-08-04 13:56:58 -0300

    devCommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
-// import { useEffect } from "react";
-// import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  console.log(data);
-
-  // useEffect(() => {
-  //   client.query({ query: GET_LESSONS_QUERY }).then((response) => {
-  //     console.log(response.data);
-  //   });
-  // }, []);
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
-  );
+  return <Event />;
 }
 
 export default App;
