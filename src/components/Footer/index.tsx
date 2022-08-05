a5c0ba1e078b3f7f948760db7bea537ef956fe29
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-08-05 09:51:10 -0300

    devCommit

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Rocketseat } from "../Rocketseat";
 export function Footer() {
   return (
     <>
-      <div className="bg-black w-full h-[16vh] flex flex-col justify-center items-center border-t border-gray-400 md:flex-row md:h-20 md:justify-around">
+      <div className="bg-black w-full h-[16vh] flex flex-col justify-center items-center border-t border-gray-400 md:flex-row md:h-20 md:justify-around md:fixed bottom-0">
         <div className="flex flex-col gap-5 items-center md:flex-row">
           <Rocketseat />
           <span className="text-gray-400">
